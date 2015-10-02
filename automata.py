7f36a4253f2e98cf3405684a038fdb01d5bdb375
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2015-10-03 00:15:44 +0200

    Initial commit

---
+++ automata.py
@@ -0,0 +1,63 @@
+"""
+Implementation of several spatial models running on a lattice
+"""
+
+import numpy as np
+
+from progressbar import ProgressBar
+
+
+class CellularAutomaton(object):
+    """ General CA class
+    """
+
+    def __init__(self, lattice):
+        self.lattice = lattice
+
+    def apply_rule(self, mat):
+        """ Return the new lattice
+        """
+        raise NotImplementedError
+
+    def iterate(self, steps):
+        """ Yield each step up to `steps` of the current CA
+        """
+        yield self.lattice
+        pbar = ProgressBar()
+        for _ in pbar(range(steps)):
+            tmp = self.apply_rule(self.lattice)
+            yield tmp
+            self.lattice = tmp
+
+class GameOfLife(CellularAutomaton):
+    """ Simulate Conway's Game of Life
+    """
+
+    def apply_rule(self, mat):
+        """ Apply one Game of Life iteration to the given system state
+        """
+        def get_neighbor_num(pos):
+            """ Get number of neighbors in Moore neighborhood of given position
+            """
+            r, c = pos
+            nr = r+1 if r+1 < mat.shape[0] else 0
+            nc = c+1 if c+1 < mat.shape[1] else 0
+
+            return sum([
+                mat[r-1, c-1], mat[r-1, c], mat[r-1, nc],
+                mat[r, c-1],                mat[r, nc],
+                mat[nr, c-1], mat[nr, c], mat[nr, nc]
+            ])
+
+        next_mat = np.zeros(mat.shape)
+        for row in range(mat.shape[0]):
+            for col in range(mat.shape[1]):
+                num = get_neighbor_num((row, col))
+                if mat[row, col] == 1:
+                    if num >= 2 and num <= 3:
+                        next_mat[row, col] = 1
+                else:
+                    if num == 3:
+                        next_mat[row, col] = 1
+
+        return next_mat
---
+++ main.py
@@ -0,0 +1,72 @@
+"""
+Create lattices, simulate models on them and animate the result
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as anime
+
+from automata import GameOfLife
+
+def add_glider(pos, mat):
+    """ Add glider to state at given position (row, col)
+    """
+    row, col = pos
+
+    mat[row, col-1] = 1
+    mat[row+1, col] = 1
+    mat[row+1, col+1] = 1
+    mat[row, col+1] = 1
+    mat[row-1, col+1] = 1
+
+    return mat
+
+def animate(evolution, out_file='out.gif'):
+    """ Create animation from list of images
+    """
+    fig = plt.figure()
+    plt.axis('off')
+
+    img = plt.imshow(evolution[0], cmap='Greys', interpolation='nearest')
+    def update_func(i):
+        """ Generate frame `i` of the animation
+        """
+        img.set_array(evolution[i])
+        return img,
+
+    anim = anime.FuncAnimation(
+        fig, update_func,
+        range(len(evolution)),
+        interval=100
+        #blit=True
+    )
+    plt.show()
+
+    anim.save(out_file, writer='imagemagick')
+
+def setup_lattice():
+    """ Populate lattice with its initial configuration
+    """
+    mat = np.zeros((50, 50))
+    mat = add_glider((7, 2), mat)
+
+    return mat
+
+def simulate(Model):
+    """ Simulate given model
+    """
+    lattice = setup_lattice()
+
+    evolution = []
+    for grid in GameOfLife(lattice).iterate(1000):
+        evolution.append(grid)
+
+    animate(evolution)
+
+def main():
+    """ Have fun :-)
+    """
+    simulate(GameOfLife)
+
+if __name__ == '__main__':
+    main()
